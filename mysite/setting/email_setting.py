ae42c348e7158842628e661dfb0772717bef4a82
somebody2012 <443135581@qq.com>
2021-02-06 20:58:19 +0800

    1

--- mysite/setting/email_setting.py
+++ mysite/setting/email_setting.py
@@ -9,5 +9,5 @@ EMAIL_HOST_PASSWORD = 'pdagirgkgrqubggc'  # 开启POP3/SMTP服务
 SERVER_EMAIL = '443135581@qq.com'  # 与发件箱一致
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # ADMINS = [('John', '443135581@qq.com')]  # 邮件接收人，可以有多个
-ADMINS = [('John', '443135581@qq.com')]  # 邮件接收人，可以有多个
+ADMINS = [('nobody', '443135581@qq.com')]  # 邮件接收人，可以有多个
 EMAIL_USE_TLS = True  # 这里必须是 True，否则发送不成功
